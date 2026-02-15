31c599aa83e41cd79104725c8aed159d7579a460
Arpith Joseph <arpithjoseph2005@gmail.com>
2026-02-15 23:01:55 +0530

    added pattern printer code

--- src/PatternPrinting.java
+++ src/PatternPrinting.java
@@ -1,5 +1,16 @@
 import java.util.Scanner;
-
 public class PatternPrinting {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for(int i = 1; i<=a; i++){
+            System.out.print("* ");
+            if(i==a)
+                System.out.println();
+        }
+        for(int j = 1; j<=a; j++){
+            System.out.println("*");
+        }
+    }
 }
+
