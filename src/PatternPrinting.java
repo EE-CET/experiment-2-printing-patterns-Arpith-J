eae0a6c8ad8b5fcfcb539c07656e2e6b785677d6
Arpith Joseph <arpithjoseph2005@gmail.com>
2026-02-15 23:04:56 +0530

    check 2

--- src/PatternPrinting.java
+++ src/PatternPrinting.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 public class PatternPrinting {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        for(int i = 1; i<=a; i++){
+        int n = sc.nextInt();
+        for(int i = 1; i<=n; i++){
             System.out.print("* ");
-            if(i==a)
+            if(i==n)
                 System.out.println();
         }
-        for(int j = 1; j<=a; j++){
+        for(int j = 1; j<=n; j++){
             System.out.println("*");
         }
     }
